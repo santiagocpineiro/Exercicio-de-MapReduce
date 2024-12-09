62151bd84a39b4f9b5157356b2747744c12296cd
santiagocpineiro <santiagocpineiro@gmail.com>
2024-12-09 21:32:04 +0100

    Fixed error

--- mapreduce/exercicio1_1/modified_mapper.py
+++ mapreduce/exercicio1_1/modified_mapper.py
@@ -10,6 +10,6 @@ import sys
 
 for line in sys.stdin:
     data = line.strip().split("\t")
-    if len(data) != 6:
+    if len(data) == 6:
         date, time, store, item, cost, payment = data
         print(store+"\t"+cost)
